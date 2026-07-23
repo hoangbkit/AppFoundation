f4c3e1a0a1002a7d2dcd4ca823b32bf3dc233d5c
Hoang Nguyen <hoangbkit@gmail.com>
2026-07-23 11:48:22 +0700

    Test Promo Video Studio Demo project

--- Examples/Demo/DemoTests/DemoTests.swift
+++ Examples/Demo/DemoTests/DemoTests.swift
@@ -117,4 +117,28 @@ final class DemoTests: XCTestCase {
             ]
         )
     }
+
+    func testPromoVideoDemoRegistersTheCompleteStory() {
+        let settings = DemoPromoVideoSettings()
+        let project = DemoPromoVideoProject.make(settings: settings)
+        let identifiers = project.scenes.map(\.id)
+
+        XCTAssertEqual(identifiers, DemoPromoVideoProject.sceneIDs)
+        XCTAssertEqual(identifiers.count, 6)
+        XCTAssertEqual(Set(identifiers).count, identifiers.count)
+        XCTAssertEqual(project.defaultPresetID, PromoVideoOutputPreset.verticalFullHD.id)
+        XCTAssertEqual(project.defaultFrameRate, .fps30)
+        XCTAssertGreaterThan(project.totalDuration, 10)
+    }
+
+    func testPromoVideoTimelineUsesOverlappingTransitions() {
+        let settings = DemoPromoVideoSettings()
+        let project = DemoPromoVideoProject.make(settings: settings)
+        let starts = project.sceneStartTimes
+
+        XCTAssertEqual(starts.first, 0)
+        XCTAssertEqual(starts.count, project.scenes.count)
+        XCTAssertLessThan(starts[1], project.scenes[0].duration)
+        XCTAssertGreaterThan(project.totalDuration, starts.last ?? 0)
+    }
 }

6043abd2167c86e6faf489433df7a5231144b264
Hoang Nguyen <hoangbkit@gmail.com>
2026-07-24 11:28:58 +0700

    Adopt dedicated Mac Studio windows

--- Examples/Demo/DemoMac/DemoMacApp.swift
+++ Examples/Demo/DemoMac/DemoMacApp.swift
@@ -4,10 +4,32 @@ import SwiftUI
 @MainActor
 struct DemoMacApp: App {
   var body: some Scene {
-    WindowGroup {
+    Window("AppFoundation for Mac", id: MacDemoWindowID.home) {
       MacHomeView()
-        .frame(minWidth: 980, minHeight: 680)
+        .frame(minWidth: 900, minHeight: 620)
     }
-    .defaultSize(width: 1180, height: 780)
+    .defaultSize(width: 1080, height: 720)
+    .windowResizability(.contentMinSize)
+    .commands {
+      MacDemoCommands()
+    }
+
+    Window("Screenshot Studio", id: MacDemoWindowID.screenshotStudio) {
+      MacStudioWindow {
+        MacScreenshotStudioDemoView()
+      }
+      .frame(minWidth: 1120, minHeight: 720)
+    }
+    .defaultSize(width: 1320, height: 860)
+    .windowResizability(.contentMinSize)
+
+    Window("Promo Video Studio", id: MacDemoWindowID.promoVideoStudio) {
+      MacStudioWindow {
+        MacPromoVideoStudioDemoView()
+      }
+      .frame(minWidth: 1120, minHeight: 720)
+    }
+    .defaultSize(width: 1320, height: 860)
+    .windowResizability(.contentMinSize)
   }
 }

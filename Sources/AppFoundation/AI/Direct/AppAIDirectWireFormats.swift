031f486a0728182d0fd8487948217212dac2de5f
Hoang Nguyen <hoangbkit@gmail.com>
2026-08-03 22:30:26 +0700

    align direct AI wire formats

---
+++ Sources/AppFoundation/AI/Direct/AppAIDirectWireFormats.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension OpenAIResponsesRequest.Input {
+    enum CodingKeys: String, CodingKey {
+        case type
+        case role
+        case content
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode("message", forKey: .type)
+        try container.encode(role, forKey: .role)
+        try container.encode(content, forKey: .content)
+    }
+}
+
+extension GeminiRequest.GenerationConfig {
+    enum CodingKeys: String, CodingKey {
+        case temperature
+        case maxOutputTokens
+        case responseMimeType
+        case responseJsonSchema
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(temperature, forKey: .temperature)
+        try container.encodeIfPresent(maxOutputTokens, forKey: .maxOutputTokens)
+        try container.encodeIfPresent(responseMimeType, forKey: .responseMimeType)
+        try container.encodeIfPresent(responseSchema, forKey: .responseJsonSchema)
+    }
+}
